1b11da815deb4a830a418d0c6c00e37840661039
Sergey <sergilio-p@yandex.ru>
2023-02-15 10:50:45 +0300

    Сделал таймер

--- js/script.js
+++ js/script.js
@@ -4,24 +4,22 @@ window.addEventListener('DOMContentLoaded', () => {
   const tabsContent = document.querySelectorAll('.tabcontent');
   const tabsParent = document.querySelector('.tabheader__items');
 
+  function hideTabContent() {
+    tabsContent.forEach((item) => {
+      item.classList.add('hide');
+      item.classList.remove('show', 'fade');
+    });
 
-function hideTabContent () {
-  tabsContent.forEach(item => {
-    item.classList.add('hide');
-    item.classList.remove('show', 'fade');
-  });
-  
-  tabs.forEach(item => {
-    item.classList.remove('tabheader__item_active');
-  });
-}
+    tabs.forEach((item) => {
+      item.classList.remove('tabheader__item_active');
+    });
+  }
 
-function showTabContent(i = 0) {
-  tabsContent[i].classList.add('show', 'fade');
-  tabsContent[i].classList.remove('hide');
-  tabs[i].classList.add('tabheader__item_active');
-}
-
+  function showTabContent(i = 0) {
+    tabsContent[i].classList.add('show', 'fade');
+    tabsContent[i].classList.remove('hide');
+    tabs[i].classList.add('tabheader__item_active');
+  }
 
   hideTabContent();
   showTabContent();
@@ -29,15 +27,72 @@ function showTabContent(i = 0) {
   tabsParent.addEventListener('click', (e) => {
     const target = e.target;
 
-    if(target && target.classList.contains('tabheader__item')) {
+    if (target && target.classList.contains('tabheader__item')) {
       tabs.forEach((item, i) => {
-        if(target == item) {
-            hideTabContent();
-            showTabContent(i);
+        if (target == item) {
+          hideTabContent();
+          showTabContent(i);
         }
-      })
+      });
     }
-  })
+  });
+
+  // ************************ 2 Создаем таймер обратного отсчета на сайте *************************
+
+   const deadline = '2023-02-25';
+
+
+   function getTimeRemaining(endTime) {
+    const t = Date.parse(endTime) - Date.parse(new Date());
+    const days = Math.floor(t / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((t / (1000 * 60 * 60) % 24));
+    const minutes = Math.floor((t / 1000 / 60) % 60);
+    const seconds = Math.floor((t / 1000) % 60);
+
+    return {
+      'total': t,
+      'days': days,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds,
+    }
+   }
+
+   function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return `0${num}`;                                                                  
+    } else {
+      return num;
+    }
+   }
+
+   function setClock(selector, endTime) {
+    const timer = document.querySelector(selector);
+    const days = timer.querySelector('#days');
+    const hours = timer.querySelector('#hours');
+    const minutes = timer.querySelector('#minutes');
+    const seconds = timer.querySelector('#seconds');
+    const timeInterval = setInterval(updateClock, 1000);
+
+    updateClock();
+
+    function updateClock() {
+      const t = getTimeRemaining(endTime)
+
+      days.textContent = getZero(t.days);
+      hours.textContent = getZero(t.hours);
+      minutes.textContent = getZero(t.minutes);
+      seconds.textContent = getZero(t.seconds);
+
+      if(t.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+   }
+
+   setClock('.timer', deadline);
+
+});
 
 
 
@@ -47,4 +102,17 @@ function showTabContent(i = 0) {
 
 
 
-})
+// const now = new Date();
+
+// console.log(now.setHours(18));
+// console.log(now)
+
+
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getDate());
+// console.log(now.getDay());
+// console.log(now.getUTCHours());
+
+// console.log(now.getTimezoneOffset());
+// console.log(now.getTime());
